59f6a7e8235aa9c31f9951583f117f9f1f8e4161
Frankie Lam <frankiesllam@gmail.com>
2018-09-15 18:57:35 -0400

    Hi

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from amadeus import Client, ResponseError
 import pprint
 import requests
 import pycountry
-from json import loads
+from json import loads, dumps
 pp = pprint.PrettyPrinter(indent=4)
 
 amadeus = Client(
@@ -12,57 +12,67 @@ amadeus = Client(
 
 APIkey = "deruRte5Y9yrs4eK59paEuSZ9mGbGX0G"
 
+def get_from_url(url):
+    data = requests.get(url)
+    data = loads(data.content)
+    return data
 
-def max_price_search(max_price, ori): #returns list_of_destinations, each element is [airport,city,state,country,price,importance]
+def get_result_from_url(url):
+    return get_from_url(url)["results"]
+
+def extract_airport_info(destination_info):
+    latitude_of_airport = float(destination_info["location"]["latitude"])
+    longtitude_of_airport = float(destination_info["location"]["longitude"])
+    name_of_airport = destination_info["name"]
+    importance = destination_info["aircraft_movements"]
+    name_of_des_city = destination_info["city_name"]
+    name_of_des_state = destination_info["state"]
+    name_of_country = pycountry.countries.get(alpha_2 = destination_info["country"]).name
+    return {"latitude_of_airport": latitude_of_airport, "longtitude_of_airport": longtitude_of_airport, "name_of_airport": name_of_airport, "name_of_des_city": name_of_des_city, "name_of_des_state": name_of_des_state, "name_of_country": name_of_country, "importance": importance}
+
+def master_search(max_price, ori): #returns list_of_destinations, each element is [lat, long, airport,city,state,country,price,importance]
     list_of_destinations = []
     try:
         url = "https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?origin=" + str(ori) + "&apikey=" + APIkey + "&max_price=" + str(max_price)
         #print(url)
-        response = requests.get(url)
-        response = loads(response.content)
+        #response = requests.get(url)
+        #response = loads(response.content)
 
         
-        for x in response["results"]:
+        for x in get_result_from_url(url):
             des = x["destination"]
             dep = x["departure_date"]
             ret = x["return_date"]
             air = x["airline"]
             price = x["price"]
             
-
-
-            
             low_fare_url = ("https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?origin=" + ori 
                             + "&destination=" + des + "&departure_date=" + dep + "&return_by=" + ret 
                             +"T23:59&apikey=" + APIkey + "&include_airline=" + air + "&number_of_results=1")
-            low_fare_result = requests.get(low_fare_url)
+            #low_fare_result = requests.get(low_fare_url)
             #print(low_fare_url)
-            low_fare_result = loads(low_fare_result.content)["results"]
-            #pp.pprint(low_fare_result)
+            low_fare_result = get_result_from_url(low_fare_url)
+           
 
             des_airport = low_fare_result[0]["itineraries"][0]["outbound"]["flights"][0]["destination"]["airport"]
-
             destination_url = "https://api.sandbox.amadeus.com/v1.2/location/" + des_airport + "?apikey=" + APIkey
-            destination_info = requests.get(destination_url)
-            destination_info = loads(destination_info.content)
+            #destination_info = requests.get(destination_url)
+            #destination_info = loads(destination_info.content)
+            destination_info = get_from_url(destination_url)
             destination_info = destination_info["airports"][0]
-            name_of_airport = destination_info["name"]
-            importance = destination_info["aircraft_movements"]
-            name_of_des_city = destination_info["city_name"]
-            name_of_des_state = destination_info["state"]
-            name_of_country = pycountry.countries.get(alpha_2 = destination_info["country"]).name
-            to_print = [name_of_airport, name_of_des_city, name_of_des_state, name_of_country, price, importance]
-            list_of_destinations.append(to_print)
-            print(','.join(map(str,to_print)))
 
-            #low_fare_url = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?origin="
-            #            + str(ori) + "&apikey=" + APIkey + "&max_price=" + str(max_price)
-            #            + "&departure_date=" + dep + "&return_date=" + ret
-            #print(x["destination"] + "," + x["price"])
-        # => [{'type': 'checkin-link', 'id': '1XEN-GBWeb', 'href': 'https://www....
+            airport_info = extract_airport_info(destination_info)
+            
+            airport_info["price"] = float(price)
+
+            list_of_destinations.append(airport_info)
+            
+
+            
     except ResponseError as error:
         print(error)
-    return list_of_destinations
+    return dumps(list_of_destinations)
 
-pp.pprint(max_price_search("100", "BOS"))
+
+pp.pprint(master_search("100", "BOS"))
 
