2ae6834c2664d80d8e3458f953344ac0b65a470b
LucasS98 <lucasfernansalvador@outlook.com>
2018-09-15 22:02:39 -0400

    JavaScript update
    It does everything Frankie code does, but in beautiful JS.

--- app/main.js
+++ app/main.js
@@ -1,44 +1,83 @@
-// First, create an object containing LatLng and population for each city.
-var citymap = {
-	chicago: {
-	  center: {lat: 41.878, lng: -87.629},
-	  population: 2714856
-	},
-	newyork: {
-	  center: {lat: 40.714, lng: -74.005},
-	  population: 8405837
-	},
-	losangeles: {
-	  center: {lat: 34.052, lng: -118.243},
-	  population: 3857799
-	},
-	vancouver: {
-	  center: {lat: 49.25, lng: -123.1},
-	  population: 603502
-	}
-};
+var map = null;
 
 function initMap() {
 	// Create the map.
-	var map = new google.maps.Map(document.getElementById('map'), {
-	  zoom: 4,
-	  center: {lat: 37.090, lng: -95.712},
+	  map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 3,
+	  center: {lat: 0, lng: 0},
 	  mapTypeId: 'terrain'
 	});
+}
 
-	// Construct the circle for each value in citymap.
-	// Note: We scale the area of the circle based on the population.
-	for (var city in citymap) {
-		// Add the circle for this city to the map.
-		var cityCircle = new google.maps.Circle({
-			strokeColor: '#008000',
-			strokeOpacity: 1,
-			strokeWeight: 2,
-			fillColor: '#008000',
-			fillOpacity: 1,
-			map: map,
-			center: citymap[city].center,
-			radius: Math.sqrt(citymap[city].population) * 100
+$(document).ready(function() {
+	var APIkey = "deruRte5Y9yrs4eK59paEuSZ9mGbGX0G";
+	
+	var colors = ["00ff00", "33ff00", "66ff00", "99ff00", "ccff00", "ffff00", "ffcc00"];
+	
+	var city;
+	
+	function getDestination(code){
+		$.ajax({
+			type: 'GET',
+			url: "https://api.sandbox.amadeus.com/v1.2/location/" + code + "?apikey=" + APIkey
+		}).done(function(response) {
+			var total =  0;
+			for (var i=0; i<response.airports.length; i++){
+				total = total + response.airports[i].aircraft_movements;
+			}	
+			
+			city = {
+				name: response.city.name,
+				state: response.city.state,
+				country: response.city.country,
+				center: {lat:  response.city.location.latitude, lng: response.city.location.longitude},
+				movement: total
+			}
 		});
 	}
-}
+	
+	
+	$('#submit').click(function() {
+		let origin = "BOS";
+		let price = 1000;
+	
+		$.ajax({
+			type: 'GET',
+			url: "https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?origin=" + origin + "&apikey=" + APIkey + "&max_price=" + price
+		}).done(function(response) {
+			for (var i=0; i<response.results.length; i++){
+				var city = getDestination(response.results[i].destination);
+				// Add the circle for this city to the map.
+				var cost = response.results[i].price;
+				
+				$.ajax({
+					type: 'GET',
+					url: "https://api.sandbox.amadeus.com/v1.2/location/" + response.results[i].destination + "?apikey=" + APIkey
+				}).done(function(sec_response) {
+					var total =  0;
+					for (var i=0; i<sec_response.airports.length; i++){
+						total = total + sec_response.airports[i].aircraft_movements;
+					}				
+					var city = {
+						name: sec_response.city.name,
+						state: sec_response.city.state,
+						country: sec_response.city.country,
+						center: {lat:  sec_response.city.location.latitude, lng: sec_response.city.location.longitude},
+						movement: total
+					}
+				
+					var cityCircle = new google.maps.Circle({
+						strokeColor: '#008000',
+						strokeOpacity: 1,
+						strokeWeight: 2,
+						fillColor: '#008000',
+						fillOpacity: 0.6,
+						map: map,
+						center: city.center,
+						radius: Math.sqrt(city.movement) * 100
+					});
+				});
+			}
+		});
+	});
+});
